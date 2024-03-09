3d2f679a50869c6626e8fcf8bfa14542518f0c86
vikas-pmt <vikas.ranjan@permi.tech>
2024-03-09 17:04:50 +0530

    added typecript code

---
+++ typescript/dataTypes.ts
@@ -0,0 +1,23 @@
+// let id: number = 5;
+// let firstname: string = 'danny';
+// let hasDog: boolean = true;
+
+// let unit: number; // Declare variable without assigning a value
+// unit = 5;
+
+// let name:string;
+
+
+
+
+// name=34      //Shows error because name should store string datatype
+
+
+let id = 7; // TS knows it's a number
+let firstname = 'angular'; // TS knows it's a string
+let hasDog = true; // TS knows it's a boolean
+
+// hasDog = 'yes'; // ERROR
+
+
+
